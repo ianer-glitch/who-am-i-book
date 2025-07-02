a4423f060caee0456e822ee192f28a576139bce4
ianer-glitch <ianrepker@gmail.com>
2025-07-02 05:09:12 -0300

    refactor: configured hashMap for project list

--- src/app/portfolio/components/projectsPostItList/useProjectList.ts
+++ src/app/portfolio/components/projectsPostItList/useProjectList.ts
@@ -1,76 +1,70 @@
-import { useTranslation } from "@/shared/locales"
-import { IProjectPostIt } from "../projectPostIt/ProjectPostIt"
-import { useEffect, useState } from "react"
+import { useTranslation } from "@/shared/locales";
+import { IProjectPostIt } from "../projectPostIt/ProjectPostIt";
+import { useEffect, useState } from "react";
+import { resources } from "@/shared/locales/languages";
 
+export const useProjectList = () => {
+  const { currentLanguage } = useTranslation();
 
-export const useProjectList= () =>{
-    const {currentLanguage} = useTranslation()
+  const [projects, setProjects] = useState([] as IProjectPostIt[]);
 
-    const [projects, setProjects] = useState([] as IProjectPostIt[])
+  const [currentProject, setCurrentProject] = useState({
+    description: "",
+    title: "",
+    desingLink: "",
+    publishedLink: "",
+    repoBackEndLink: "",
+    repoFrontEndLink: "",
+    color: "yellow",
+  } as IProjectPostIt);
 
-    const [currentProject , setCurrentProject] = useState({
-      description: "",
-      title: "",
-      desingLink: "",
-      publishedLink: "",
-      repoBackEndLink: "",
-      repoFrontEndLink: "",
-      color:"yellow"
-    } as IProjectPostIt)
+  const projectsByLanguage: Record<keyof typeof resources, IProjectPostIt[]> = {
+    ptBr: [
+      {
+        description: "a",
+        title: "b",
+        desingLink: ".",
+        publishedLink: ".",
+        repoBackEndLink: ".",
+        repoFrontEndLink: ".",
+        color: "blue",
+      },
+      {
+        description: "a",
+        title: "b",
+        desingLink: ".",
+        publishedLink: ".",
+        repoBackEndLink: ".",
+        repoFrontEndLink: ".",
+        color: "pink",
+      },
+    ],
+    enUs: [
+      {
+        description: "english",
+        title: "b",
+        desingLink: ".",
+        publishedLink: ".",
+        repoBackEndLink: ".",
+        repoFrontEndLink: ".",
+        color: "blue",
+      },
+      {
+        description: "a",
+        title: "b",
+        desingLink: ".",
+        publishedLink: ".",
+        repoBackEndLink: ".",
+        repoFrontEndLink: ".",
+        color: "pink",
+      },
+    ],
+  };
 
-    const portugueseProjects: IProjectPostIt[] = [
-    {
-      description: "a",
-      title: "b",
-      desingLink: ".",
-      publishedLink: ".",
-      repoBackEndLink: ".",
-      repoFrontEndLink: ".",
-      color:"blue"
-    },
-     {
-      description: "a",
-      title: "b",
-      desingLink: ".",
-      publishedLink: ".",
-      repoBackEndLink: ".",
-      repoFrontEndLink: ".",
-      color:"pink"
-    },
-    ];
+  useEffect(() => {
+    setProjects(projectsByLanguage[currentLanguage]);
+    setCurrentProject(projectsByLanguage[currentLanguage][0]);
+  }, [currentLanguage]);
 
-  const englishProjects: IProjectPostIt[] = [
-    {
-      description: "english",
-      title: "b",
-      desingLink: ".",
-      publishedLink: ".",
-      repoBackEndLink: ".",
-      repoFrontEndLink: ".",
-      color:"blue"
-    },
-     {
-      description: "a",
-      title: "b",
-      desingLink: ".",
-      publishedLink: ".",
-      repoBackEndLink: ".",
-      repoFrontEndLink: ".",
-      color:"pink"
-    },
-    ];
-
-    useEffect(()=>{
-        if(currentLanguage == "ptBr"){
-            setProjects(portugueseProjects)
-            setCurrentProject(portugueseProjects[0])
-        }else{
-            setProjects(englishProjects)
-            setCurrentProject(englishProjects[0])
-        }
-    },[currentLanguage])
-
-
-  return {projects,currentProject,setCurrentProject}
-    
-}
+  return { projects, currentProject, setCurrentProject };
+};
