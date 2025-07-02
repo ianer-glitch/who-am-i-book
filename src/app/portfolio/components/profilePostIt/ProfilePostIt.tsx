f24b06490fdf28c72d76bcee6c145db36d9d8936
ianer-glitch <ianrepker@gmail.com>
2025-07-02 05:15:17 -0300

    feat: created github button

--- src/app/portfolio/components/profilePostIt/ProfilePostIt.tsx
+++ src/app/portfolio/components/profilePostIt/ProfilePostIt.tsx
@@ -1,6 +1,7 @@
 import { LinkedInButton } from "@/shared/components/molecules/linkedInButton/LinkedInButton"
 import { PostIt } from "@/shared/components/atoms/postIt/PostIt"
 import { useTranslation } from "@/shared/locales"
+import { GitHubButton } from "@/shared/components/molecules/gitHubButton/gitHubButton"
 
 
 export const ProfilePostIt = () => {
@@ -9,6 +10,7 @@ return (
     <PostIt className="w-full" color="yellow">
         <h1 style={{fontFamily:"var(--font-londrina-shadow)"}}>{t("profilePostItTitle")}</h1>
         <LinkedInButton/>
+        <GitHubButton/>
     </PostIt>
 )    
 }
---
+++ src/shared/components/molecules/gitHubButton/gitHubButton.tsx
@@ -0,0 +1,12 @@
+import { IconLinkButton } from "../../atoms/iconLinkButton/IconLinkButton"
+
+export const GitHubButton = () => {
+    return(
+        <IconLinkButton
+            icon="pi pi-github"
+            iconColor=""
+            className="bg-white"
+            link="https://github.com/ianer-glitch"
+        />
+    )
+}
